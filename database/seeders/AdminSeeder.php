b678432dad243761df815953192c3d852a41cba1
Thismecoks <fajarardian262@gmail.com>
2025-09-10 14:56:48 +0700

    update

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -11,38 +11,21 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         $admins = [
-            [
-                'username' => env('ADMIN1_USERNAME', 'admin1'),
-                'password' => env('ADMIN1_PASSWORD', 'admin123'),
-            ],
-            [
-                'username' => env('ADMIN2_USERNAME', 'admin2'),
-                'password' => env('ADMIN2_PASSWORD', 'admin123'),
-            ],
-            [
-                'username' => env('ADMIN3_USERNAME', 'admin3'),
-                'password' => env('ADMIN3_PASSWORD', 'admin123'),
-            ],
-            [
-                'username' => env('ADMIN4_USERNAME', 'admin4'),
-                'password' => env('ADMIN4_PASSWORD', 'admin123'),
-            ],
-            [
-                'username' => env('ADMIN5_USERNAME', 'admin5'),
-                'password' => env('ADMIN5_PASSWORD', 'admin123'),
-            ],
+            ['username' => 'admin1', 'password' => 'admin123'],
+            ['username' => 'admin2', 'password' => 'admin123'],
+            ['username' => 'admin3', 'password' => 'admin123'],
+            ['username' => 'admin4', 'password' => 'admin123'],
+            ['username' => 'admin5', 'password' => 'admin123'],
         ];
 
         foreach ($admins as $admin) {
-            if ($admin['username'] && $admin['password']) {
-                User::updateOrCreate(
-                    ['username' => $admin['username']], // cari username
-                    [
-                        'password' => Hash::make($admin['password']),
-                        'role'     => 'admin',
-                    ]
-                );
-            }
+            User::updateOrCreate(
+                ['username' => $admin['username']],
+                [
+                    'password' => Hash::make($admin['password']),
+                    'role'     => 'admin',
+                ]
+            );
         }
     }
 }
